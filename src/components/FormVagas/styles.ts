e2b234d9ce904c2117a5e5aca8a705dc3b14cf40
Vitor Hugo <vitoritouguimaraes@gmail.com>
2024-09-04 23:35:27 -0400

    finalizando exercicio

--- src/components/FormVagas/styles.ts
+++ src/components/FormVagas/styles.ts
@@ -7,20 +7,37 @@ const Formulario = styled.form`
   padding: 32px;
   border-radius: 12px;
   margin-top: 40px;
+
+  @media (max-width: 450px) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 10px;
+
+    input {
+      font-size: 20px;
+    }
+
+    button {
+      margin-left: 0px;
+    }
+  }
 `
 export default Formulario
 
-export const Campo = styled.input`
-  padding: 0 16px;
-  outline-color: var(--cor-principal);
-`
 export const Botao = styled.button`
   background-color: var(--cor-principal);
   border: 1px solid var(--cor-principal);
   height: 40px;
+  width: 100%;
   padding: 0 16px;
   font-size: 18px;
   color: var(--cor-secundaria);
   margin-left: 8px;
   cursor: pointer;
+`
+export const Campo = styled.input`
+  padding: 0 16px;
+  outline-color: var(--cor-principal);
+  width: 100%;
 `
